c1d4185a817a3ba94cdda8e9ae9c0024e83fc558
Felippe Fernandes <felippe1000@hotmail.com>
2022-07-16 16:07:18 -0300

    FEAT: Local Storage Functions

---
+++ src/helpers/localStorageSaves.jsx
@@ -0,0 +1,24 @@
+// Email
+export const getEmail = () => JSON.parse(localStorage.getItem('user'));
+
+if (!getEmail) {
+  localStorage.setItem('user', JSON.stringify({ email: 'teste@xpInc.com' }));
+}
+
+export const saveEmail = (email) => {
+  localStorage.setItem('user', JSON.stringify({ email }));
+};
+//
+
+// Hour
+export const getDate = () => JSON.parse(localStorage.getItem('date'));
+
+if (!getDate) {
+  localStorage.setItem('date', JSON.stringify({ day: '16 de julho de 2022', hour: '15:18' }));
+}
+
+export const saveDate = (day, hour) => {
+  localStorage.setItem('user', JSON.stringify({ day, hour }));
+};
+//
+
