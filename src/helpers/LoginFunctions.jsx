a8dcf221c0fc9758c0e6d08fb729dd6a348be299
Felippe Fernandes <felippe1000@hotmail.com>
2022-07-17 12:13:47 -0300

    FIX: Function logic

--- src/helpers/LoginFunctions.jsx
+++ src/helpers/LoginFunctions.jsx
@@ -1,5 +1,10 @@
 let regex = new RegExp('[a-z0-9]+@[a-z]+\\.[a-z]{2,3}');
 
 export const validateEmail = (email) => {
-    return (regex.test(email))
+    const emailValidation = regex.test(email);
+    if (emailValidation === true) {
+        return true;
+    } else {
+        return false;
+    }
 };
---
+++ src/pages/Wallet/Wallet.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import styles from './styles.module.css';
+import Header from '../../components/Header/Header'
+
+function Wallet() {
+
+  return (
+    <Header />
+  );
+}
+
+export default Wallet;
