5574a90ed477f98531fc197a717f43f22f0a112f
Felippe Fernandes <felippe1000@hotmail.com>
2022-07-17 12:13:29 -0300

    FEAT: Headers Component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import { getEmail } from '../../helpers/localStorageSaves';
+import styles from './styles.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserAstronaut, faGripLinesVertical } from '@fortawesome/free-solid-svg-icons';
+
+function Header() {
+    const getUser = () => {
+        return getEmail().email
+    }
+
+    useEffect(() => {
+        getEmail()
+    },);
+
+    return (
+        <div className={styles.HeaderComponent}>
+            <div className={styles.User}>
+                <p><FontAwesomeIcon icon={faUserAstronaut}/> {getUser()}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
