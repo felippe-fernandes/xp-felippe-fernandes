87cb5a2fab88546d4d98b5e7a3bf3e9a4fb6d53a
Felippe Fernandes <felippe1000@hotmail.com>
2022-07-18 15:31:01 -0300

    FEAT: Checkout Buttons

--- src/components/CheckouModal/CheckouModal.jsx
+++ src/components/CheckouModal/CheckouModal.jsx
@@ -2,17 +2,26 @@ import React, { useContext } from 'react';
 import Context from '../../context/Context';
 import CheckoutTable from '../CheckoutTable/CheckoutTable';
 import styles from './styles.module.css';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faUserAstronaut } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck, faRotateLeft } from '@fortawesome/free-solid-svg-icons';
 
 function CheckouModal() {
-    // const { shareSelected } = useContext(Context)
+    const { setShowModal } = useContext(Context)
+
+    const handleBackClick = () => {
+        setShowModal(false)
+    }
 
 
     return (
         <div className={styles.CheckouModalComponent}>
             <h1>Comprar/Vender Ação</h1>
             <CheckoutTable />
+            <div className={styles.QtyInputs}></div>
+            <div className={styles.CheckoutButtons}>
+                <button onClick={handleBackClick}><FontAwesomeIcon icon={faRotateLeft}/>Voltar</button>
+                <button>Confirmar<FontAwesomeIcon icon={faCheck}/></button>
+            </div>
         </div>
     );
 }
