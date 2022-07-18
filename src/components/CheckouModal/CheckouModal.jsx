fb42a8043785a37d326da44959504ef9a2fdc22d
Felippe Fernandes <felippe1000@hotmail.com>
2022-07-18 15:44:35 -0300

    FEAT: Sell and buy Buttons

--- src/components/CheckouModal/CheckouModal.jsx
+++ src/components/CheckouModal/CheckouModal.jsx
@@ -4,6 +4,7 @@ import CheckoutTable from '../CheckoutTable/CheckoutTable';
 import styles from './styles.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck, faRotateLeft } from '@fortawesome/free-solid-svg-icons';
+import { Form, InputGroup } from 'react-bootstrap';
 
 function CheckouModal() {
     const { setShowModal } = useContext(Context)
@@ -15,12 +16,29 @@ function CheckouModal() {
 
     return (
         <div className={styles.CheckouModalComponent}>
-            <h1>Comprar/Vender Ação</h1>
-            <CheckoutTable />
-            <div className={styles.QtyInputs}></div>
+            <div className={styles.FirstDiv}>
+                <h1>Comprar/Vender Ação</h1>
+                <CheckoutTable />
+            </div>
+            <div className={styles.QtyInputs}>
+                <InputGroup className={styles.Inputs}>
+                    <InputGroup.Text id="BuyButton">Comprar</InputGroup.Text>
+                    <Form.Control
+                        placeholder="Informe o valor"
+                        aria-label="Buy Input"
+                    />
+                </InputGroup>
+                <InputGroup className={styles.Inputs}>
+                    <InputGroup.Text id="SellButton">Vender</InputGroup.Text>
+                    <Form.Control
+                        placeholder="Informe o valor"
+                        aria-label="Sell Input"
+                    />
+                </InputGroup>
+            </div>
             <div className={styles.CheckoutButtons}>
-                <button onClick={handleBackClick}><FontAwesomeIcon icon={faRotateLeft}/>Voltar</button>
-                <button>Confirmar<FontAwesomeIcon icon={faCheck}/></button>
+                <button onClick={handleBackClick}><FontAwesomeIcon icon={faRotateLeft} />Voltar</button>
+                <button>Confirmar<FontAwesomeIcon icon={faCheck} /></button>
             </div>
         </div>
     );
