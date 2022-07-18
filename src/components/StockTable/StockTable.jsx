46ae6542feafd6a9548d3336c0f0454f08492fa6
Felippe Fernandes <felippe1000@hotmail.com>
2022-07-18 11:15:13 -0300

    FEAT: Stock Table /wallet

--- src/components/StockTable/StockTable.jsx
+++ src/components/StockTable/StockTable.jsx
@@ -6,32 +6,65 @@ import { faC, faV } from '@fortawesome/free-solid-svg-icons';
 
 function StockTable() {
 
-    const createTableRows = () => {
-        return shares.map((share) => (
-            <tbody key={share.name}>
-                <tr>
-                    <td>{share.name}</td>
-                    <td>{share.qty}</td>
-                    <td>{Number(share.price).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
-                    <td id={styles.ButtonsColumn}>
-                        <button id={styles.BuyButton}><FontAwesomeIcon icon={faC} /></button>
-                        <button id={styles.SellButton}><FontAwesomeIcon icon={faV} /></button>
-                    </td>
-                </tr>
-            </tbody>
+    const myShareTable = () => {
+        return shares.filter((share) => share.itHas).map((share) => (
+            <tr key={share.name}>
+                <td id={styles.NameColumn}>{share.name}</td>
+                <td id={styles.QtyColumn}>{share.qty}</td>
+                <td id={styles.PriceColumn}>{Number(share.price).toFixed(2).replace('.', ',')}</td>
+                <td id={styles.ButtonsColumn}>
+                    <button id={styles.BuyButton}><FontAwesomeIcon icon={faC} /></button>
+                    <button id={styles.SellButton}><FontAwesomeIcon icon={faV} /></button>
+                </td>
+            </tr>
         ));
     };
+
+    const otherSharesTable = () => {
+        return shares.filter((share) => !share.itHas).map((share) => (
+            <tr key={share.name}>
+                <td id={styles.NameColumn}>{share.name}</td>
+                <td id={styles.QtyColumn}>{share.qty}</td>
+                <td id={styles.PriceColumn}>{Number(share.price).toFixed(2).replace('.', ',')}</td>
+                <td id={styles.ButtonsColumn}>
+                    <button id={styles.BuyButton}><FontAwesomeIcon icon={faC} /></button>
+                    <button id={styles.SellButton} disabled={!share.itHas}><FontAwesomeIcon icon={faV} /></button>
+                </td>
+            </tr>
+        ));
+    };
+
     return (
         <table className={styles.Table}>
             <thead>
-                <tr className={styles.TableHeaderRow}>
-                    <th className={styles.TableHeaderData}>Ação</th>
-                    <th className={styles.TableHeaderData}>Qtde</th>
-                    <th className={styles.TableHeaderData}>Valor (R$)</th>
-                    <th className={styles.TableHeaderData}>Negociar</th>
+                <tr colSpan={3} className={styles.MergedHeaderRow}>
+                    <th className={styles.MergedHeaderData}>Minhas Ações:</th>
                 </tr>
             </thead>
-            {createTableRows()}
+            <tbody>
+                <tr className={styles.TableSubHeaderRow}>
+                    <th className={styles.TableSubHeaderData}>Ação</th>
+                    <th className={styles.TableSubHeaderData}>Qtde</th>
+                    <th className={styles.TableSubHeaderData}>Valor (R$)</th>
+                    <th className={styles.TableSubHeaderData}>Negociar</th>
+                </tr>
+                {myShareTable()}
+            </tbody>
+            <br />
+            <thead>
+                <tr colSpan={3} className={styles.MergedHeaderRow}>
+                    <th className={styles.MergedHeaderData}>Disponíveis para Investir:</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr className={styles.TableSubHeaderRow}>
+                    <th className={styles.TableSubHeaderData}>Ação</th>
+                    <th className={styles.TableSubHeaderData}>Qtde</th>
+                    <th className={styles.TableSubHeaderData}>Valor (R$)</th>
+                    <th className={styles.TableSubHeaderData}>Negociar</th>
+                </tr>
+                {otherSharesTable()}
+            </tbody>
         </table>
 
     );
