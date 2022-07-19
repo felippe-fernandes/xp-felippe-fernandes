9df593ce8b059f5d798e218e095a7f3a9db3bb7d
Felippe Fernandes <felippe1000@hotmail.com>
2022-07-19 09:23:03 -0300

    FEAT: Payment Page

---
+++ src/pages/Payment/Payment.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+function Payment() {
+  return (
+    <div className={styles.PaymentPage}>Payment</div>
+  );
+}
+
+export default Payment;
