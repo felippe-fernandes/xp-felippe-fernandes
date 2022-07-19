4930f2a972de06a1bb6561af4e09da4d25d586e0
Felippe Fernandes <felippe1000@hotmail.com>
2022-07-19 10:19:45 -0300

    FEAT: Payment Page

--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import styles from './styles.module.css';
+import Header from '../../components/Header/Header'
+import TransactionForms from '../../components/TransactionsForms/TransactionForms';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faMoneyBillTransfer } from '@fortawesome/free-solid-svg-icons';
+
 
 function Payment() {
+
+
   return (
-    <div className={styles.PaymentPage}>Payment</div>
+    <div className={styles.PaymentPage}>
+        <Header />
+        <TransactionForms />
+    </div>
   );
 }
 
