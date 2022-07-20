5d5d46b306f6602da4abd8fde79665631ab931e8
Felippe Fernandes <felippe1000@hotmail.com>
2022-07-20 13:13:59 -0300

    FIX: Header Blur

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import styles from './styles.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserAstronaut, faMoneyBillWave, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { userInfos } from '../../helpers/Infos';
+import { getEmail } from '../../helpers/localStorageSaves';
 
 function Header() {
     const [seeBalance, setSeeBalance] = useState(false)
@@ -16,8 +17,8 @@ function Header() {
     useEffect(() => {
         const checkDisbale = () => {
             if (!seeBalance) {
-                setBlurId('none');
-            } else { setBlurId('blur'); }
+                setBlurId('blur');
+            } else { setBlurId('none'); }
         };
         checkDisbale();
     }, [seeBalance]);
@@ -44,15 +45,15 @@ function Header() {
     return (
         <div className={styles.HeaderComponent}>
             <div className={styles.User}>
-                <p><FontAwesomeIcon icon={faUserAstronaut} /> {userInfos.user}</p>
+                <p><FontAwesomeIcon icon={faUserAstronaut} /> {getEmail().email.toLowerCase()}</p>
             </div>
             <div className={styles.BalanceDiv}>
                 <button onClick={handleClick} >
-                    {seeBalance ? <FontAwesomeIcon icon={faEyeSlash} /> :
-                        <FontAwesomeIcon icon={faEye} />}
+                    {seeBalance ? <FontAwesomeIcon icon={faEye} /> :
+                        <FontAwesomeIcon icon={faEyeSlash} />}
                 </button>
                 <div className={styles.Balance} id={styles[`${blurId}`]}>
-                    <>{seeBalance ? blurBalance : normalBalance}</>
+                    <>{seeBalance ? normalBalance : blurBalance}</>
                 </div>
             </div>
         </div>
