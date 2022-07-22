eaa3d83fde5f785e466f10c110c23444fbaa3be8
Felippe Fernandes <felippe1000@hotmail.com>
2022-07-22 12:35:45 -0300

    FEAT: Wallet page tests

--- src/Tests/Wallet.test.jsx
+++ src/Tests/Wallet.test.jsx
@@ -1,120 +1,116 @@
 import { fireEvent, screen } from '@testing-library/react';
 import React from 'react';
-import Login from '../pages/Login/Login';
-import renderWithRouter from '../Utils/RenderWithRouter';
+import Wallet from '../pages/Wallet/Wallet';
+import renderWithRouter from './Utils/RenderWithRouter';
 
-describe('Teste a página de Login', () => {
-  it('Teste se a página possui dois inputs', () => {
-    renderWithRouter(<Login />);
+describe('Teste a página de Wallet', () => {
+  it('Teste se a página possui uma tabela', () => {
+    renderWithRouter(<Wallet />);
 
-    const inputs = screen.getAllByRole('textbox');
-    expect(inputs).toHaveLength(2);
+    const table = screen.getByRole('table');
+    expect(table).toBeInTheDocument();
   });
-  it('Teste se o input de email possui o placeholder E-mail', () => {
-    renderWithRouter(<Login />);
+  it('Teste se a tabela possui todos os cabeçalhos', () => {
+    renderWithRouter(<Wallet />);
 
-    const getEmailInput = screen.getByRole('textbox', {
-      name: /email/i
-    });
-    const emailInputPlaceholder = getEmailInput.getAttribute('placeholder');
-
-    expect(emailInputPlaceholder).toBe('E-mail');
+    const tableColumnHeader = screen.getAllByRole('tableheader');
+    expect(tableColumnHeader).toHaveLength(11);
   });
-  it('Teste se o input de password possui o placeholder Senha', () => {
-    renderWithRouter(<Login />);
+  // it('Teste se o input de password possui o placeholder Senha', () => {
+  //   renderWithRouter(<Login />);
 
-    const getPasswordInput = screen.getByRole('textbox', {
-      name: /password/i
-    });
-    const passwordInputPlaceholder = getPasswordInput.getAttribute('placeholder');
+  //   const getPasswordInput = screen.getByRole('textbox', {
+  //     name: /password/i
+  //   });
+  //   const passwordInputPlaceholder = getPasswordInput.getAttribute('placeholder');
 
-    expect(passwordInputPlaceholder).toBe('Senha');
-  });
+  //   expect(passwordInputPlaceholder).toBe('Senha');
+  // });
 
-  it('Teste se existe um botão', () => {
-    renderWithRouter(<Login />);
+  // it('Teste se existe um botão', () => {
+  //   renderWithRouter(<Login />);
 
-    const button = screen.getAllByRole('button');
+  //   const button = screen.getAllByRole('button');
 
-    expect(button).toHaveLength(1);
-  });
-  it('Teste se o botão tem o texto Acessar', () => {
-    renderWithRouter(<Login />);
+  //   expect(button).toHaveLength(1);
+  // });
+  // it('Teste se o botão tem o texto Acessar', () => {
+  //   renderWithRouter(<Login />);
 
-    const accessButton = screen.getByRole('button', {
-      name: /acessar/i
-    });
+  //   const accessButton = screen.getByRole('button', {
+  //     name: /acessar/i
+  //   });
 
-    expect(accessButton).toBeInTheDocument();
-  });
-  it('Teste se o botão começa desabilitado', () => {
-    renderWithRouter(<Login />);
+  //   expect(accessButton).toBeInTheDocument();
+  // });
+  // it('Teste se o botão começa desabilitado', () => {
+  //   renderWithRouter(<Login />);
 
-    const accessButton = screen.getByRole('button', {
-      name: /acessar/i
-    });
+  //   const accessButton = screen.getByRole('button', {
+  //     name: /acessar/i
+  //   });
 
-    expect(accessButton).toBeDisabled();
-  });
-  it('Teste se o botão habilita depois de um email e uma senha válidos', () => {
-    renderWithRouter(<Login />);
+  //   expect(accessButton).toBeDisabled();
+  // });
+  // it('Teste se o botão habilita depois de um email e uma senha válidos', () => {
+  //   renderWithRouter(<Login />);
 
-    const getEmailInput = screen.getByRole('textbox', {
-      name: /email/i
-    });
-    const getPasswordInput = screen.getByRole('textbox', {
-      name: /password/i
-    });
-    const accessButton = screen.getByRole('button', {
-      name: /acessar/i
-    });
+  //   const getEmailInput = screen.getByRole('textbox', {
+  //     name: /email/i
+  //   });
+  //   const getPasswordInput = screen.getByRole('textbox', {
+  //     name: /password/i
+  //   });
+  //   const accessButton = screen.getByRole('button', {
+  //     name: /acessar/i
+  //   });
 
-    fireEvent.change(getEmailInput, { target: { value: 'teste@xpinc.com' } })
-    fireEvent.change(getPasswordInput, { target: { value: '123456' } })
+  //   fireEvent.change(getEmailInput, { target: { value: 'teste@xpinc.com' } })
+  //   fireEvent.change(getPasswordInput, { target: { value: '123456' } })
 
-    expect(accessButton).toBeEnabled();
-  });
-  it('Teste se o botão continua desabilitado com um email e uma senha inválidos', () => {
-    renderWithRouter(<Login />);
+  //   expect(accessButton).toBeEnabled();
+  // });
+  // it('Teste se o botão continua desabilitado com um email e uma senha inválidos', () => {
+  //   renderWithRouter(<Login />);
 
-    const getEmailInput = screen.getByRole('textbox', {
-      name: /email/i
-    });
-    const getPasswordInput = screen.getByRole('textbox', {
-      name: /password/i
-    });
-    const accessButton = screen.getByRole('button', {
-      name: /acessar/i
-    });
+  //   const getEmailInput = screen.getByRole('textbox', {
+  //     name: /email/i
+  //   });
+  //   const getPasswordInput = screen.getByRole('textbox', {
+  //     name: /password/i
+  //   });
+  //   const accessButton = screen.getByRole('button', {
+  //     name: /acessar/i
+  //   });
 
-    fireEvent.change(getEmailInput, { target: { value: 'teste@xpinc' } })
-    fireEvent.change(getPasswordInput, { target: { value: '123456' } })
-    expect(accessButton).toBeDisabled();
+  //   fireEvent.change(getEmailInput, { target: { value: 'teste@xpinc' } })
+  //   fireEvent.change(getPasswordInput, { target: { value: '123456' } })
+  //   expect(accessButton).toBeDisabled();
 
-    fireEvent.change(getEmailInput, { target: { value: 'teste@xpinc.com' } })
-    fireEvent.change(getPasswordInput, { target: { value: '1234' } })
-    expect(accessButton).toBeDisabled();
+  //   fireEvent.change(getEmailInput, { target: { value: 'teste@xpinc.com' } })
+  //   fireEvent.change(getPasswordInput, { target: { value: '1234' } })
+  //   expect(accessButton).toBeDisabled();
 
-    fireEvent.change(getEmailInput, { target: { value: 'teste@xpinc' } })
-    fireEvent.change(getPasswordInput, { target: { value: '1234' } })
-    expect(accessButton).toBeDisabled();
-  });
-  it('Teste se ao clicar no botão de acessar o usuario é redirecionado para a /wallet', () => {
-    const { history } = renderWithRouter(<Login />)
+  //   fireEvent.change(getEmailInput, { target: { value: 'teste@xpinc' } })
+  //   fireEvent.change(getPasswordInput, { target: { value: '1234' } })
+  //   expect(accessButton).toBeDisabled();
+  // });
+  // it('Teste se ao clicar no botão de acessar o usuario é redirecionado para a /wallet', () => {
+  //   const { history } = renderWithRouter(<Login />)
 
-    const getEmailInput = screen.getByRole('textbox', {
-      name: /email/i
-    });
-    const getPasswordInput = screen.getByRole('textbox', {
-      name: /password/i
-    });
-    const accessButton = screen.getByRole('button', {
-      name: /acessar/i
-    });
+  //   const getEmailInput = screen.getByRole('textbox', {
+  //     name: /email/i
+  //   });
+  //   const getPasswordInput = screen.getByRole('textbox', {
+  //     name: /password/i
+  //   });
+  //   const accessButton = screen.getByRole('button', {
+  //     name: /acessar/i
+  //   });
 
-    fireEvent.change(getEmailInput, { target: { value: 'teste@xpinc.com' } })
-    fireEvent.change(getPasswordInput, { target: { value: '123456' } })
-    fireEvent.click(accessButton)
-    expect(history.location.pathname).toBe('/wallet')
-  });
+  //   fireEvent.change(getEmailInput, { target: { value: 'teste@xpinc.com' } })
+  //   fireEvent.change(getPasswordInput, { target: { value: '123456' } })
+  //   fireEvent.click(accessButton)
+  //   expect(history.location.pathname).toBe('/wallet')
+  // });
 });
