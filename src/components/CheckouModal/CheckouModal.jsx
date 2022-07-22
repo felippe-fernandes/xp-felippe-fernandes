924138ab41c25585fc3a89af64223803bc94e15d
Felippe Fernandes <felippe1000@hotmail.com>
2022-07-22 01:04:44 -0300

    FEAT: Alert Button

--- src/components/CheckouModal/CheckouModal.jsx
+++ src/components/CheckouModal/CheckouModal.jsx
@@ -3,7 +3,7 @@ import Context from '../../context/Context';
 import CheckoutTable from '../CheckoutTable/CheckoutTable';
 import styles from './styles.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck, faRotateLeft, faXmarkSquare } from '@fortawesome/free-solid-svg-icons';
+import { faCheck, faExclamation, faRotateLeft, faXmarkSquare } from '@fortawesome/free-solid-svg-icons';
 import { Form, InputGroup } from 'react-bootstrap';
 import { buyAndSellActions } from '../../helpers/transactionsFunctions'
 import SuccessScreen from '../SuccessScreen/SuccessScreen';
@@ -14,6 +14,7 @@ function CheckouModal() {
     const [confirmButtonDisable, setConfirmButtonDisable] = useState(true);
     const [buyQty, setBuyQty] = useState(0);
     const [sellQty, setSellQty] = useState(0);
+    const [transactionPossible, setTransactionPossible] = useState('nok');
 
     const handleBackClick = () => {
         setShowModal(false)
@@ -34,10 +35,13 @@ function CheckouModal() {
                 buyQty <= shareSelected.qtyAvailable &&
                 buyQty * shareSelected.price < balance) {
                 setConfirmButtonDisable(false);
+                setTransactionPossible('ok')
             } else if (sellQty > 0 && sellQty <= shareSelected.qty) {
                 setConfirmButtonDisable(false);
+                setTransactionPossible('ok')
             } else {
                 setConfirmButtonDisable(true)
+                setTransactionPossible('nok')
             }
         };
         enableConfirmButton();
@@ -54,27 +58,37 @@ function CheckouModal() {
                 </div>
                 <CheckoutTable />
             </div>
-            <div className={styles.QtyInputs}>
-                <InputGroup className={styles.Inputs}>
-                    <InputGroup.Text id={styles.BuyButton}>Comprar</InputGroup.Text>
-                    <Form.Control
-                        onChange={({ target }) => setBuyQty(Number(target.value))}
-                        placeholder="Informe a quantidade"
-                        aria-label="Buy Input"
-                        type='number'
-                        disabled={sellQty > 0}
-                    />
-                </InputGroup>
-                <InputGroup className={styles.Inputs}>
-                    <InputGroup.Text id={styles.SellButton}>Vender</InputGroup.Text>
-                    <Form.Control
-                        onChange={({ target }) => setSellQty(Number(target.value))}
-                        placeholder="Informe a quantidade"
-                        aria-label="Sell Input"
-                        type='number'
-                        disabled={shareSelected.qty === 0 || buyQty > 0}
-                    />
-                </InputGroup>
+            <div className={styles.FormArea}>
+                <div className={styles.QtyInputs}>
+                    <InputGroup className={styles.Inputs}>
+                        <InputGroup.Text id={styles.BuyButton}>Comprar</InputGroup.Text>
+                        <Form.Control
+                            onChange={({ target }) => setBuyQty(Number(target.value))}
+                            placeholder="Informe a quantidade"
+                            aria-label="Buy Input"
+                            type='number'
+                            disabled={sellQty > 0}
+                        />
+                    </InputGroup>
+                    <InputGroup className={styles.Inputs}>
+                        <InputGroup.Text id={styles.SellButton}>Vender</InputGroup.Text>
+                        <Form.Control
+                            onChange={({ target }) => setSellQty(Number(target.value))}
+                            placeholder="Informe a quantidade"
+                            aria-label="Sell Input"
+                            type='number'
+                            disabled={shareSelected.qty === 0 || buyQty > 0}
+                        />
+                    </InputGroup>
+                </div>
+                <div className={styles.TotalTransaction} id={styles[`${transactionPossible}`]}>
+                    <p className={styles.TotalValue}>Valor Total</p>
+                    {transactionPossible === 'ok' ?
+                        <p>R$ {Number((sellQty + buyQty) * shareSelected.price).toFixed(2).replace('.', ',')}</p> :
+                        <p><FontAwesomeIcon icon={faExclamation} size='2xl'/></p>
+                    }
+                </div>
+
             </div>
             <div className={styles.CheckoutButtons}>
                 <button onClick={handleBackClick}><FontAwesomeIcon icon={faRotateLeft} />Voltar</button>
