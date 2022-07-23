d49dfd41598b0cb1b62ad4ce07ae94026527ecdc
Felippe Fernandes <felippe1000@hotmail.com>
2022-07-23 13:38:50 -0300

    FEAT: SuccessScreenComponet Tests

--- src/Tests/successModal.test.jsx
+++ src/Tests/successModal.test.jsx
@@ -1,4 +1,4 @@
-import { fireEvent, screen, waitFor } from "@testing-library/react";
+import { fireEvent, screen } from "@testing-library/react";
 import React from "react";
 import SuccessScren from "../components/SuccessScreen/SuccessScreen";
 import renderWithRouter from "./Utils/RenderWithRouter";
